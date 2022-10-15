a9d2b555cc0ccf64e633d9b0969c757dbf470a5c
Alejandro Perez <alexgt9@gmail.com>
2022-10-15 10:47:33 +0200

    Now it´s for contacts

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ var cors = require('cors');
 
 const PORT = process.env.PORT || 5000
 
-const todos = {};
-todos['aleh'] = {};
-todos['aleh']['911d7404-f927-46b8-bde9-759be745061d'] = {'id': '911d7404-f927-46b8-bde9-759be745061d', 'text': 'Agarra la sombrilla', 'completed': false, 'author': 'aleh', 'createdAt': new Date() };
-todos['aleh']['911d7404-f927-46b8-bde9-759be745061b'] = {'id': '911d7404-f927-46b8-bde9-759be745061b', 'text': 'Agarra el bañador', 'completed': false, 'author': 'aleh', 'createdAt': new Date() };
-todos['aleh']['911d7404-f927-46b8-bde9-759be745061a'] = {'id': '911d7404-f927-46b8-bde9-759be745061a', 'text': 'Ponte a bailar', 'completed': false, 'author': 'aleh', 'createdAt': new Date() };
+const contacts = {};
+contacts['aleh'] = {};
+contacts['aleh']['911d7404-f927-46b8-bde9-759be745061d'] = {id: '911d7404-f927-46b8-bde9-759be745061d', name: 'Paco Pil', phone: '777666555', email: 'paco.pil@example.com', 'createdAt': new Date() };
+contacts['aleh']['911d7404-f927-46b8-bde9-759be745061b'] = {id: '911d7404-f927-46b8-bde9-759be745061b', name: 'Maria Gil', phone: '666888444', email: 'maria.gil@example.com', 'createdAt': new Date() };
+contacts['aleh']['911d7404-f927-46b8-bde9-759be745061a'] = {id: '911d7404-f927-46b8-bde9-759be745061a', name: 'Jul Til', phone: '676999999', email: 'jul.til@example.com', 'createdAt': new Date() };
 
 express()
   .use(cors())
@@ -18,59 +18,61 @@ express()
   .get('/', (req, res) => {
       res.redirect('README.html');
   })
-  .get('/users/:username/todos', (req, res) => {
+  .get('/users/:username/contacts', (req, res) => {
       const username = req.params.username;
 
-      if (!todos[username]) {
-          todos[username] = {};
+      if (!contacts[username]) {
+          contacts[username] = {};
       }
-      res.json(Object.values(todos[username]))
+      res.json(Object.values(contacts[username]))
   })
-  .post('/users/:username/todos', (req, res) => {
+  .post('/users/:username/contacts', (req, res) => {
       const username = req.params.username;
 
-      if (!todos[username]) {
-          todos[username] = {};
+      if (!contacts[username]) {
+          contacts[username] = {};
       }
 
-      let newTodo = {
-        "text": req.body.text,
+      let newContact = {
+        "name": req.body.name,
         "id": uuidv4(),
-        "completed": false,
-        "author": username,
+        "phone": req.body.phone,
+        "email": req.body.email,
         "createdAt": new Date()
       };
 
-      todos[username][newTodo.id] = newTodo;
-      res.json(newTodo);
+      contacts[username][newContact.id] = newContact;
+      res.json(newContact);
   })
-  .patch('/users/:username/todos/:id', (req, res) => {
+  .patch('/users/:username/contacts/:id', (req, res) => {
       const username = req.params.username;
       const id = req.params.id;
-      if (!todos[username] || !todos[username][id]) {
+      if (!contacts[username] || !contacts[username][id]) {
           res.status(404).json({ "error": 'Not found'});
 
           return;
       }
 
-      const completed = req.body.completed ?? todos[username][id].completed;
-      const text = req.body.text || todos[username][id].text;
+      const name = req.body.name || contacts[username][id].name;
+      const phone = req.body.phone || contacts[username][id].phone;
+      const email = req.body.email || contacts[username][id].email;
 
-      todos[username][id].completed = completed;
-      todos[username][id].text = text;
+      contacts[username][id].name = name;
+      contacts[username][id].phone = phone;
+      contacts[username][id].email = email;
 
-      res.json(todos[username][id]);
+      res.json(contacts[username][id]);
   })
-  .delete('/users/:username/todos/:id', (req, res) => {
+  .delete('/users/:username/contacts/:id', (req, res) => {
       const username = req.params.username;
       const id = req.params.id;
-      if (!todos[username] || !todos[username][id]) {
+      if (!contacts[username] || !contacts[username][id]) {
           res.status(404).json({ "error": 'Not found'});
 
           return;
       }
 
-      delete todos[username][id];
+      delete contacts[username][id];
 
       res.json({});
   })
